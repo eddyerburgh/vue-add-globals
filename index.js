1c776d8f55172cac9b15371ab106bf7f0194b260
Edd Yerburgh <edward.yerburgh@gmail.com>
2017-05-03 22:43:43 +0100

    [Fix] Replace arrow function with anonymous function

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 module.exports = function (globals) {
   return {
     install: function (Vue) {
-      Object.keys(globals).forEach(key => {
+      Object.keys(globals).forEach(function (key) {
         Vue.prototype[key] = globals[key]
       })
     }
